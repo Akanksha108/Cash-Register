9da6d75dd72fde41e002ebef799a2d20fcf6eb14
Akanksha <83341547+Akanksha108@users.noreply.github.com>
2021-07-24 15:49:33 +0530

    added function for minimum notes

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-bill_amount = document.getElementById("bill_amount");
 
-cash_given = document.getElementById("cash_given");
+let bill_amount = document.getElementById("bill_amount");
 
-submit_button = document.getElementById("submit_btn");
+let cash_given = document.getElementById("cash_given");
 
-para = document.getElementById("demo");
+let submit_button = document.getElementById("submit_btn");
+
+let para = document.getElementById("demo");
+
+let output_demo = document.getElementById("demo1");
+
 
 function calculate(event){
+ 
+    event.preventDefault();
 
-    var cashToBeGiven = bill_amount.value - cash_given.value;
-    console.log(cashToBeGiven); 
-    
-    para.innerText = cashToBeGiven;
+    let cashToBeGiven = bill_amount.value - cash_given.value;
+
+    para.innerHTML = cashToBeGiven;
+
+    let notes = [2000, 500, 100, 20, 10, 5, 1];
+    let currencyCount = Array(8).fill(0);
+    //console.log(currencyCount);
+
+    for(let i = 0; i < 8; i++){
+        if(cashToBeGiven  >= notes[i]){
+            currencyCount[i] = Math.floor(cashToBeGiven/notes[i]);
+            cashToBeGiven = cashToBeGiven - (currencyCount[i] * notes[i]);
+        }
+    }
+
+   document.getElementById("currency_heading").innerHTML = ("Number of notes to return: " + "<br />");
+    for(let i = 0; i < 8; i++){
+        if(currencyCount[i] != 0){
+            output_demo.innerHTML += notes[i] + " : " + currencyCount[i] + "<br />";
+        }
+    }
 };
 
 submit_button.addEventListener('click', calculate);
