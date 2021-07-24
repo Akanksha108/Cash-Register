e1533196d7020052d65202ba08cce319d4e915c2
Akanksha <83341547+Akanksha108@users.noreply.github.com>
2021-07-24 16:36:58 +0530

    added table to display output

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ let cash_given = document.getElementById("cash_given");
 
 let submit_button = document.getElementById("submit_btn");
 
-let para = document.getElementById("demo");
-
-let output_demo = document.getElementById("demo1");
+let output_demo = document.getElementById("demo");
 
 
 function calculate(event){
@@ -15,14 +13,12 @@ function calculate(event){
     event.preventDefault();
 
     let cashToBeGiven = bill_amount.value - cash_given.value;
-
-    para.innerHTML = cashToBeGiven;
+    console.log(cashToBeGiven);
 
     let notes = [2000, 500, 100, 20, 10, 5, 1];
-    let currencyCount = Array(8).fill(0);
-    //console.log(currencyCount);
+    let currencyCount = Array(7).fill(0);
 
-    for(let i = 0; i < 8; i++){
+    for(let i = 0; i < 7; i++){
         if(cashToBeGiven  >= notes[i]){
             currencyCount[i] = Math.floor(cashToBeGiven/notes[i]);
             cashToBeGiven = cashToBeGiven - (currencyCount[i] * notes[i]);
@@ -30,9 +26,34 @@ function calculate(event){
     }
 
    document.getElementById("currency_heading").innerHTML = ("Number of notes to return: " + "<br />");
-    for(let i = 0; i < 8; i++){
+    for(let i = 0; i < 7; i++){
         if(currencyCount[i] != 0){
             output_demo.innerHTML += notes[i] + " : " + currencyCount[i] + "<br />";
+
+            if(notes[i] == 1){
+                document.getElementById("demo1").innerHTML += currencyCount[i] + "<br />";
+            }
+            else if(notes[i] === 5){
+                document.querySelector("#demo2").innerHTML += currencyCount[i] + "<br />";
+            }
+            else if(notes[i] == 10){
+                document.getElementById("demo3").innerHTML += currencyCount[i] + "<br />";
+            }
+            else if(notes[i] == 20){
+                document.getElementById("demo4").innerHTML += currencyCount[i] + "<br />";
+            }
+            else if(notes[i] == 100){
+                document.getElementById("demo5").innerHTML += currencyCount[i] + "<br />";
+            }
+            else if(notes[i] == 500){
+                document.getElementById("demo6").innerHTML += currencyCount[i] + "<br />";
+            }
+            else if(notes[i] == 2000){
+                document.getElementById("demo7").innerHTML += currencyCount[i] + "<br />";
+            }
+            else{
+                console.log("Please enter correct input");
+            }
         }
     }
 };
