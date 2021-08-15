b314098e498e8999a02af66bc8f94b969478dc94
Akanksha <83341547+Akanksha108@users.noreply.github.com>
2021-08-15 23:01:53 +0530

    updated input validation

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ let cash_given = document.getElementById("cash_given");
 
 let submit_button = document.getElementById("submit_btn");
 
-let output_demo = document.getElementById("demo");
-
 let table = document.querySelector("#table");
 
 let displayError = document.getElementById("error");
@@ -16,8 +14,11 @@ function calculate(event){
  
     event.preventDefault();
 
-    if(cash_given.value >= bill_amount.value){
-        let cashToBeGiven = cash_given.value - bill_amount.value;
+    const cashGivenValue = parseInt(cash_given.value);
+    const billAmountValue = parseInt(bill_amount.value);
+
+    if(cashGivenValue >= billAmountValue){
+        let cashToBeGiven = cashGivenValue - billAmountValue;
 
         let notes = [2000, 500, 100, 20, 10, 5, 1];
         let currencyCount = Array(7).fill(0);  //will return an array of 0's
@@ -42,7 +43,6 @@ function calculate(event){
 
         for(let i = 0; i < 7; i++){
             if(currencyCount[i] != 0){
-                // output_demo.innerHTML += notes[i] + " : " + currencyCount[i] + "<br />";
 
                 if(notes[i] == 1){
                     document.getElementById("demo1").innerHTML = currencyCount[i];
